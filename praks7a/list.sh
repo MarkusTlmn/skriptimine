84796f8d7e647e910d307ceb32948dc5a1f47b77
Markus Tellmann <markus.tellmann@voco.ee>
2024-09-24 02:54:01 -0400

    yl

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -9,13 +9,16 @@ read -p "Sisestage kataloogi nimi: " kl
 find "$kl/" -print0 | while IFS= read -d '' item; do
 	if [ -f "$item" ]; then
 		echo "$(basename "$item"): fail"
+		failid=$((failid + 1))
 	elif [ -d "$item" ]; then
 		echo "$(basename "$item"): kataloog"
+		lingid=$((lingid + 1))
  	elif [ -L "$item" ]; then
 		echo "$(basename "$item"): list"
+		kataloogid=$((kataloogid + 1))
 	else
 		echo "$(basename "$item"): tundmatu"
 	fi
 done
-
+echo "Kokku on $failid faili, $kataloogid kataloogi ja $lingid linki."
 # skripti lõpp
